d6c1a881fa1ee7c5ef56901c7cb7edf1a3f4c220
ironman <arshikulsum123@gmail.com>
2023-04-20 23:06:01 +0530

    pushing assignment-invitaion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Invitation from './Invitation.js'
+import { Fragment } from "react"
 
 function App() {
+   
+   const sub="Birthday party Invitation"
+    const mail= " jaGdish@gmail.com"
+    const name= "jagdish"
+    const guest1 = "saurav"
+    const guest2 = "ritu" 
+    const guest3 = "karthik"
+    const venue= "green park avenue" 
+   
   return (
-    <div className="App">
-      <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-        <h2>Benifits of using react...</h2>
-          <ol>
-            <li>Component-based architecture</li>
-            <li>Virtual DOM for efficient updates</li>
-            <li>Rich ecosystem and community</li>
-            <li>Cross-platform development</li>
-            <li>Strong community support</li>
-          </ol>
-          <button>Get Started</button>  
-      </header>
-    </div>
+    <Fragment>
+
+        <Invitation name={name} mail={mail} sub={sub} guest1={guest1} guest2={guest2} guest3={guest3} venue={venue}/>
+    
+    </Fragment>  
   );
 }
 
