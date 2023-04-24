bb09cb53465a752c79336605bdf4bf7abd64075d
ironman <arshikulsum123@gmail.com>
2023-04-25 01:01:56 +0530

    pushing props assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Invitation from './Invitation.js'
-import { Fragment } from "react"
+import { Card1 } from './allCard';
+import { Card2 } from './allCard';
+import { Card3 } from './allCard';
+
 
 function App() {
-   
-   const sub="Birthday party Invitation"
-    const mail= " jaGdish@gmail.com"
-    const name= "jagdish"
-    const guest1 = "saurav"
-    const guest2 = "ritu" 
-    const guest3 = "karthik"
-    const venue= "green park avenue" 
-   
-  return (
-    <Fragment>
 
-        <Invitation name={name} mail={mail} sub={sub} guest1={guest1} guest2={guest2} guest3={guest3} venue={venue}/>
-    
-    </Fragment>  
+  
+  return (  
+    <>
+       <Card1 />
+       <Card2 />
+       <Card3 />
+        
+  
+  
+
+
+
+    </>   
   );
 }
 
---
+++ src/allCard.js
@@ -0,0 +1,38 @@
+import Card from './card';
+
+export function Card1 (){
+    return(
+        <Card para="somebody who ensures a good user 
+        experience executing both a great visual design 
+        and code"
+       desg="Frontened Developer"
+       name="KULSUM ARSHI"
+       link="https://t3.ftcdn.net/jpg/02/45/34/90/360_F_245349044_TMxmWxPpnSzeuauvvQnuFe03ueXgS57m.jpg"
+       />
+    )
+}
+
+export function Card2 (){
+    return(
+        <Card para=" UI/UX design aims to create a
+         positive user experience that encourages 
+         customers to stick with a brand or product.
+         "
+       desg="Ui-Ux Designer"
+       name="AALIA"
+       link="https://easydrawingguides.com/wp-content/uploads/2023/01/how-to-draw-a-black-girl-cartoon-featured-image-1200.png"
+       />
+    )
+}
+
+export function Card3 (){
+    return(
+        <Card para="who build and maintain the 
+        mechanisms that process data and perform 
+        actions on websites."
+       desg="Backened Developer"
+       name="AAMIR"
+       link="https://e0.pxfuel.com/wallpapers/471/565/desktop-wallpaper-cartoon-dp-boy-for-whatsapp-cute-alone-etc-thumbnail.jpg"
+       />
+    )
+}
